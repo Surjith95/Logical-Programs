b97d247061543bb4e362bac9f0a325dafe4a4b1a
Surjith <manoharsurjith@gmail.com>
2023-12-12 13:38:40 +0530

    Changed the for loop values from 5 to 6

--- src/logical/Fibonacci.java
+++ src/logical/Fibonacci.java
@@ -1,10 +1,12 @@
+package logical;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
 
 		int a=0, b=1, c=0;
 		
-		for(int i=0; i<5; i++) {
+		for(int i=0; i<6; i++) {
 			c = a+b;
 			System.out.println(c);
 			a=b;
